2401e69df8bda43113553e25e714f2ab4a364494
willianchu <willianchu@hotmail.com>
2022-10-18 20:40:33 -0700

    api update answers

--- src/server.js
+++ src/server.js
@@ -1,5 +1,23 @@
 import  express  from "express";
 
+let answers = [
+  {
+    name: "disc-test",
+    answers: [],
+    index: 0,
+  },
+  {
+    name: "soft-skills",
+    answers: [],
+    index: 0,
+  },
+  {
+    name: "knowledge",
+    answers: [],
+    index: 0,
+  },
+];
+
 const app = express();
 app.use(express.json());
 
@@ -10,6 +28,18 @@ app.post("/hello", (req, res) => {
 app.get('/hello/:name', (req, res) => {
   const { name } = req.params;
   res.send(`Hello ${name}!`);
+});
+
+app.put('/api/questionnaire/:name/answers', (req, res) => {
+  const { name } = req.params;
+  const currentAnswers = answers.find((a) => a.name === name);
+  if (currentAnswers) {
+    currentAnswers.index = currentAnswers.index + 1;
+    // currentAnswers.answers.push(req.body);
+    res.send(`The ${name} questionnaire has answered ${currentAnswers.index} questions`);
+  } else {
+    res.status(404).send(`The ${name} questionnaire was not found`);
+  }
 });
 
 
